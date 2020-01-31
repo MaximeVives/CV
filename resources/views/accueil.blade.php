cc7056e551b6e852f87838434cfa6399523dd9a3
Ouii <44499746+Ouii@users.noreply.github.com>
2020-01-31 19:12:21 +0100

    layout

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends ('layouts\layout_accueil')
 
-        <title>Neumorphism-Design</title>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <p>Oui</p>
-    </body>
-</html>
+@section ('content')
+    <p>OUI</p>
+@endsection
